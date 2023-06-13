af6524c691c394ec8596a57d756927caa09fb413
Ganesh-kaarthik <110884217+Ganesh-kaarthik@users.noreply.github.com>
2023-06-13 23:30:17 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'https://flow-clone.onrender.com/'})
+const API = axios.create({ baseURL: 'https://dynamic-haupia-24feaa.netlify.app/'})
 
 
 API.interceptors.request.use((req) => {
